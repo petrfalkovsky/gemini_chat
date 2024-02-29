7f8841f87543bc5bc321f5f5ffb64e868557680a
petrfalkovsky <petrfalkovsky@yandex.ru>
2024-02-29 10:49:06 +0500

    feat: add configs

---
+++ lib/config.dart
@@ -0,0 +1,3 @@
+class AppConfig {
+  static const String apiKey = 'AIzaSyC_5Lds5_SgmMfoDiBPPnMwo8sCEWyROwU';
+}
---
+++ lib/ui/theme/color_constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorsConsts {
+  static const Color blue = Color.fromARGB(255, 171, 222, 244);
+  static final Color? darkBlue = Colors.blue[300];
+}
